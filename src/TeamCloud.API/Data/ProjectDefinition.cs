c31bc6bcf8ce66784ad1ba3660f85249e0630b50
Mohammed Adenwala <adenwala@outlook.com>
2020-01-10 15:46:25 -0600

    Sealed class and initialized Users list property

--- src/TeamCloud.API/Data/ProjectDefinition.cs
+++ src/TeamCloud.API/Data/ProjectDefinition.cs
@@ -11,12 +11,12 @@ using Newtonsoft.Json.Serialization;
 namespace TeamCloud.API
 {
     [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
-    public class ProjectDefinition
+    public sealed class ProjectDefinition
     {
         public string Name { get; set; }
 
         public Dictionary<string, string> Tags { get; set; } = new Dictionary<string, string>();
 
-        public List<UserDefinition> Users { get; set; }
+        public List<UserDefinition> Users { get; set; } = new List<UserDefinition>();
     }
 }
