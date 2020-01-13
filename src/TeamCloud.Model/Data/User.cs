015bb105495f378f1ebdad48b2f0fda491124cae
Mohammed Adenwala <adenwala@outlook.com>
2020-01-13 13:15:02 -0600

    Merged User and UserComparer files into User.cs

--- src/TeamCloud.Model/Data/User.cs
+++ src/TeamCloud.Model/Data/User.cs
@@ -31,4 +31,22 @@ namespace TeamCloud.Model
             RuleFor(obj => obj.Tags).NotEmpty();
         }
     }
+
+    public class UserComparer : IEqualityComparer<User>
+    {
+        public bool Equals(User x, User y)
+        {
+            if (object.ReferenceEquals(x, y))
+                return true;
+            else if (x == null || y == null)
+                return false;
+            else if (x.Id == y.Id)
+                return true;
+            else
+                return false;
+        }
+
+        public int GetHashCode(User obj)
+            => obj.Id.GetHashCode();
+    }
 }
