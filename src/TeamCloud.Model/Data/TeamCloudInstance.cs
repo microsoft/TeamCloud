c9bf9b4e3aa71985e700bfd1ef6a459f6e6e307d
Mohammed Adenwala <adenwala@outlook.com>
2020-01-13 13:14:44 -0600

    Added validation for TeamCloudInstance model

--- src/TeamCloud.Model/Data/TeamCloudInstance.cs
+++ src/TeamCloud.Model/Data/TeamCloudInstance.cs
@@ -5,13 +5,14 @@
 
 using System;
 using System.Collections.Generic;
+using FluentValidation;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
 namespace TeamCloud.Model
 {
     [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
-    public class TeamCloudInstance : IContainerDocument
+    public sealed class TeamCloudInstance : IContainerDocument
     {
         public string Id = Constants.CosmosDb.TeamCloudInstanceId;
 
@@ -26,5 +27,15 @@ namespace TeamCloud.Model
         public List<Guid> ProjectIds { get; set; } = new List<Guid>();
 
         public TeamCloudConfiguration Configuration { get; set; }
+    }
+
+    public sealed class TeamCloudInstanceValidator : AbstractValidator<TeamCloudInstance>
+    {
+        public TeamCloudInstanceValidator()
+        {
+            RuleFor(obj => obj.Users).NotEmpty();
+            RuleFor(obj => obj.ProjectIds).NotEmpty();
+            RuleFor(obj => obj.Configuration).NotEmpty();
+        }
     }
 }
