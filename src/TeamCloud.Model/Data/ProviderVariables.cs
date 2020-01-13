5724db8b38d76285051e761745a7e199152d5866
Mohammed Adenwala <adenwala@outlook.com>
2020-01-13 13:40:17 -0600

    Added validation

--- src/TeamCloud.Model/Data/ProviderVariables.cs
+++ src/TeamCloud.Model/Data/ProviderVariables.cs
@@ -4,16 +4,26 @@
  */
 
 using System.Collections.Generic;
+using FluentValidation;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
 namespace TeamCloud.Model
 {
     [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
-    public class ProviderVariables
+    public sealed class ProviderVariables
     {
         public string ProviderId { get; set; }
 
         public Dictionary<string, string> Variables { get; set; } = new Dictionary<string, string>();
+    }
+
+    public sealed class ProviderVariablesValidator : AbstractValidator<ProviderVariables>
+    {
+        public ProviderVariablesValidator()
+        {
+            RuleFor(obj => obj.ProviderId).NotEmpty();
+            RuleFor(obj => obj.Variables).NotEmpty();
+        }
     }
 }
