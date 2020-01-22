7050f1706dedd67390cdf45bd179390fb612b145
Colby Williams <ColbyLWilliams@gmail.com>
2020-01-22 11:27:10 -0500

    update nuget packages to 3.1.1

--- src/TeamCloud.Configuration/Options/AzureResourceManagerOptions.cs
+++ src/TeamCloud.Configuration/Options/AzureResourceManagerOptions.cs
@@ -9,8 +9,11 @@ namespace TeamCloud.Configuration.Options
     public class AzureResourceManagerOptions
     {
         public string ClientId { get; set; }
+        
         public string ClientSecret { get; set; }
+        
         public string TenantId { get; set; }
+        
         public string Region { get; set; }
     }
 }
