ba73941a143fa4b375cc4ad86a807b086c305bed
Colby Williams <ColbyLWilliams@gmail.com>
2020-02-27 14:50:17 -0500

    Update AzureSubscriptionPoolSelectActivity.cs

--- src/TeamCloud.Orchestrator/Orchestrations/Azure/AzureSubscriptionPoolSelectActivity.cs
+++ src/TeamCloud.Orchestrator/Orchestrations/Azure/AzureSubscriptionPoolSelectActivity.cs
@@ -4,11 +4,11 @@
  */
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
-using TeamCloud.Azure;
 using TeamCloud.Azure.Resources;
 using TeamCloud.Data;
 using TeamCloud.Model.Data;
@@ -36,18 +36,18 @@ namespace TeamCloud.Orchestrator.Orchestrations.Azure
             var subscriptionCapacityTasks = project.Type.Subscriptions
                 .Select(subscriptionId => GetSubscriptionCapacityAsync(project.Type, subscriptionId));
 
-            var subscriptionCapacity = await Task
+            IEnumerable<(Guid subscription, int capacity)> subscriptionCapacity = await Task
                 .WhenAll(subscriptionCapacityTasks)
                 .ConfigureAwait(false);
 
-            var subscriptionPick = subscriptionCapacity
-                .OrderByDescending((item) => item.Item2)
+            var (subscription, capacity) = subscriptionCapacity
+                .OrderByDescending((sub) => sub.capacity)
                 .First();
 
-            if (subscriptionPick.Item2 == 0)
+            if (capacity == 0)
                 throw new NotSupportedException($"All subscriptions reached their maximum capacity of {project.Type.SubscriptionCapacity} project of type {project.Type.Id}.");
 
-            return subscriptionPick.Item1;
+            return subscription;
         }
 
         private async Task<(Guid, int)> GetSubscriptionCapacityAsync(ProjectType projectType, Guid subscriptionId)
@@ -55,7 +55,7 @@ namespace TeamCloud.Orchestrator.Orchestrations.Azure
             var subscription = await azureResourceService
                 .GetSubscriptionAsync(subscriptionId)
                 .ConfigureAwait(false);
-            
+
             if (subscription is null)
                 return (subscriptionId, 0);
 
@@ -73,7 +73,7 @@ namespace TeamCloud.Orchestrator.Orchestrations.Azure
                     .GetInstanceCountAsync(projectType.Id, subscriptionId)
                     .ConfigureAwait(false);
 
-                return (subscriptionId, Math.Max((projectType.SubscriptionCapacity - instanceCount), 0));
+                return (subscriptionId, Math.Max(projectType.SubscriptionCapacity - instanceCount, 0));
             }
 
             return (subscriptionId, 0);
