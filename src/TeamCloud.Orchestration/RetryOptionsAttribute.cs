3c1c60b77633b29803ab8e392d0f5ab2b71fedf6
Markus Heiliger <markus.heiliger@microsoft.com>
2020-03-04 16:47:18 -0800

    added TeamCloud.Orchestration library

---
+++ src/TeamCloud.Orchestration/RetryOptionsAttribute.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Azure.WebJobs;
+
+namespace TeamCloud.Orchestration
+{
+    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+    public sealed class RetryOptionsAttribute : Attribute
+    {
+        private static readonly ConcurrentDictionary<string, RetryOptionsAttribute> Cache = new ConcurrentDictionary<string, RetryOptionsAttribute>();
+
+        internal static RetryOptionsAttribute GetByFunctionName(string functionName)
+        {
+            if (functionName is null)
+                throw new ArgumentNullException(nameof(functionName));
+
+            return Cache.GetOrAdd(functionName, (key) =>
+            {
+                var functionMethod = AppDomain.CurrentDomain.GetAssemblies()
+                    .Where(asm => !asm.IsDynamic)
+                    .SelectMany(asm => asm.GetExportedTypes().Where(type => type.IsClass))
+                    .SelectMany(type => type.GetMethods())
+                    .FirstOrDefault(method => method.GetCustomAttribute<FunctionNameAttribute>()?.Name.Equals(functionName) ?? false);
+
+                if (functionMethod is null)
+                    throw new ArgumentOutOfRangeException(nameof(functionName), $"Could not find function by name '{functionName}'");
+
+                return functionMethod.GetCustomAttribute<RetryOptionsAttribute>();
+            });
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="FunctionRetryAttribute"/>
+        /// </summary>
+        /// <param name="maxNumberOfAttempts">Sets the max number of attempts</param>
+        public RetryOptionsAttribute(int maxNumberOfAttempts)
+        {
+            if (maxNumberOfAttempts < 1)
+                throw new ArgumentException("Invalid number of attempts. Specify a value greater than zero.", nameof(maxNumberOfAttempts));
+
+            MaxNumberOfAttempts = maxNumberOfAttempts;
+        }
+
+        private TimeSpan firstRetryInterval = TimeSpan.FromMinutes(1);
+
+        /// <summary>
+        /// Gets the first retry interval
+        /// default to 1 minute
+        /// </summary>
+        public string FirstRetryInterval
+        {
+            get => firstRetryInterval.ToString();
+            set => firstRetryInterval = TimeSpan.Parse(value);
+        }
+
+        private TimeSpan maxRetryInterval = TimeSpan.FromHours(1);
+
+        /// <summary>
+        /// Gets or sets the max retry interval
+        /// defaults to 1 hour
+        /// </summary>
+        public string MaxRetryInterval
+        {
+            get => maxRetryInterval.ToString();
+            set => maxRetryInterval = TimeSpan.Parse(value);
+        }
+
+        private TimeSpan retryTimeout = TimeSpan.FromDays(1);
+
+        /// <summary>
+        /// Gets or sets the timeout for retries
+        /// defaults to 1 day
+        /// </summary>
+        public string RetryTimeout
+        {
+            get => retryTimeout.ToString();
+            set => retryTimeout = TimeSpan.Parse(value);
+        }
+
+        /// <summary>
+        /// Gets or sets the back-off coefficient
+        /// defaults to 1, used to determine rate of increase of back-off
+        /// </summary>
+        public double BackoffCoefficient { get; set; } = 1;
+
+
+        /// <summary>
+        /// Gets or sets the max number of attempts
+        /// </summary>
+        public int MaxNumberOfAttempts { get; private set; }
+    }
+}
---
+++ src/TeamCloud.Orchestration/RetryOptionsExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+
+namespace TeamCloud.Orchestration
+{
+    public static class RetryOptionsExtensions
+    {
+        private static RetryOptions GetFunctionRetryOptions(string functionName)
+        {
+            var retryAttribute = RetryOptionsAttribute.GetByFunctionName(functionName) ?? new RetryOptionsAttribute(1);
+
+            var retryOptions = new RetryOptions(TimeSpan.Parse(retryAttribute.FirstRetryInterval), retryAttribute.MaxNumberOfAttempts)
+            {
+                MaxRetryInterval = TimeSpan.Parse(retryAttribute.MaxRetryInterval),
+                RetryTimeout = TimeSpan.Parse(retryAttribute.RetryTimeout),
+                BackoffCoefficient = retryAttribute.BackoffCoefficient
+            };
+
+            return retryOptions;
+        }
+
+        public static Task CallActivityWithRetryAsync(this IDurableOrchestrationContext orchestration, string functionName, object input)
+            => orchestration.CallActivityWithRetryAsync(functionName, GetFunctionRetryOptions(functionName), input);
+
+        public static Task<TResult> CallActivityWithRetryAsync<TResult>(this IDurableOrchestrationContext orchestration, string functionName, object input)
+            => orchestration.CallActivityWithRetryAsync<TResult>(functionName, GetFunctionRetryOptions(functionName), input);
+
+        public static Task CallSubOrchestratorWithRetryAsync(this IDurableOrchestrationContext orchestration, string functionName, object input)
+            => orchestration.CallSubOrchestratorWithRetryAsync(functionName, GetFunctionRetryOptions(functionName), input);
+
+        public static Task<TResult> CallSubOrchestratorWithRetryAsync<TResult>(this IDurableOrchestrationContext orchestration, string functionName, object input)
+            => orchestration.CallSubOrchestratorWithRetryAsync<TResult>(functionName, GetFunctionRetryOptions(functionName), input);
+
+        public static Task CallSubOrchestratorWithRetryAsync(this IDurableOrchestrationContext orchestration, string functionName, string instanceId, object input)
+            => orchestration.CallSubOrchestratorWithRetryAsync(functionName, GetFunctionRetryOptions(functionName), instanceId, input);
+
+        public static Task<TResult> CallSubOrchestratorWithRetryAsync<TResult>(this IDurableOrchestrationContext orchestration, string functionName, string instanceId, object input)
+            => orchestration.CallSubOrchestratorWithRetryAsync<TResult>(functionName, GetFunctionRetryOptions(functionName), instanceId, input);
+
+    }
+}
