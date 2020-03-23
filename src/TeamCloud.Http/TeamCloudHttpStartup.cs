d5ee9adcad6b4d5b36b558075f670af98424086c
Markus Heiliger <markus.heiliger@microsoft.com>
2020-03-23 08:42:44 -0700

    Disable certificate validation for DEBUG builds

---
+++ src/TeamCloud.Http/TeamCloudHttpStartup.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using TeamCloud.Http;
+
+[assembly: FunctionsStartup(typeof(TeamCloudHttpStartup))]
+
+namespace TeamCloud.Http
+{
+    public class TeamCloudHttpStartup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            DisableCertificateValidation();
+        }
+
+        [Conditional("DEBUG")]
+        [SuppressMessage("Security", "CA5359:Do Not Disable Certificate Validation", Justification = "Only for DEBUG builds")]
+        private void DisableCertificateValidation()
+        {
+            ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+        }
+    }
+}
