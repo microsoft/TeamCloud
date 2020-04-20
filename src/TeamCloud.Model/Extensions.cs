9a32c31ce44d057963dc773e0466246795753c3b
Markus Heiliger <markus.heiliger@microsoft.com>
2020-04-20 11:00:26 -0700

    added diag support for project scope

--- src/TeamCloud.Model/Extensions.cs
+++ src/TeamCloud.Model/Extensions.cs
@@ -14,6 +14,21 @@ namespace TeamCloud.Model
 {
     public static class Extensions
     {
+        public static IDisposable BeginProjectScope(this ILogger logger, Project project)
+        {
+            if (logger is null)
+                throw new ArgumentNullException(nameof(logger));
+
+            if (project is null)
+                throw new ArgumentNullException(nameof(project));
+
+            return logger.BeginScope(new Dictionary<string, object>()
+            {
+                { "projectId", project.Id },
+                { "projectName", project.Name }
+            });
+        }
+
         public static IDisposable BeginCommandScope(this ILogger logger, ICommand command, Provider provider = default)
         {
             if (logger is null)
@@ -22,11 +37,14 @@ namespace TeamCloud.Model
             if (command is null)
                 throw new ArgumentNullException(nameof(command));
 
+            var project = command.Payload as Project;
+
             return logger.BeginScope(new Dictionary<string, object>()
             {
                 { "commandId", command.CommandId },
                 { "commandType", command.GetType().Name },
-                { "projectId", command.ProjectId },
+                { "projectId", project?.Id ?? command.ProjectId },
+                { "projectName", project?.Name },
                 { "providerId", provider?.Id }
             });
         }
