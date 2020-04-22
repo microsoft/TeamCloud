991e656124557151cc4f80e41b26bf0c030c79b7
Markus Heiliger <markus.heiliger@microsoft.com>
2020-04-22 15:01:09 -0700

    telemetry optimizations

--- src/TeamCloud.Http/Telemetry/HttpTelemetryHandler.cs
+++ src/TeamCloud.Http/Telemetry/HttpTelemetryHandler.cs
@@ -4,6 +4,7 @@
  */
 
 using System;
+using System.IdentityModel.Tokens.Jwt;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -19,21 +20,25 @@ namespace TeamCloud.Http.Telemetry
         private readonly TelemetryConfiguration telemetryConfiguration;
 
         public HttpTelemetryHandler(TelemetryConfiguration telemetryConfiguration)
-            => this.telemetryConfiguration = telemetryConfiguration ?? throw new System.ArgumentNullException(nameof(telemetryConfiguration));
+            => this.telemetryConfiguration = telemetryConfiguration ?? throw new ArgumentNullException(nameof(telemetryConfiguration));
 
         public HttpTelemetryHandler(HttpMessageHandler innerHandler, TelemetryConfiguration telemetryConfiguration = null) : base(innerHandler)
-            => this.telemetryConfiguration = telemetryConfiguration ?? throw new System.ArgumentNullException(nameof(telemetryConfiguration));
+            => this.telemetryConfiguration = telemetryConfiguration ?? throw new ArgumentNullException(nameof(telemetryConfiguration));
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
+            if (request is null)
+                throw new ArgumentNullException(nameof(request));
+
             var response = await base
                 .SendAsync(request, cancellationToken)
                 .ConfigureAwait(false);
 
-            return SendTelemetry(response);
+            return await SendTelemetryAsync(request, response)
+                .ConfigureAwait(false);
         }
 
-        private HttpResponseMessage SendTelemetry(HttpResponseMessage response)
+        private async Task<HttpResponseMessage> SendTelemetryAsync(HttpRequestMessage request, HttpResponseMessage response)
         {
             const string headerPrefix = "x-ms-ratelimit";
 
@@ -41,34 +46,76 @@ namespace TeamCloud.Http.Telemetry
             {
                 var telemetryClient = new TelemetryClient(telemetryConfiguration)
                 {
+                    // usually there should be no need to set the instrumentation key
+                    // another time, as it is already part of the telemetry configuration.
+                    // however, there are some many different ways to inititalize
+                    // a new telemetry client that the approach we use here requires 
+                    // this extra work.
+
+                    // for more information please check:
+                    // https://github.com/microsoft/ApplicationInsights-dotnet/issues/826
+
                     InstrumentationKey = telemetryConfiguration.InstrumentationKey
                 };
+
+                var identity = Guid.Empty;
+
+                if ((request.Headers.Authorization?.Scheme ?? string.Empty).Equals("bearer", StringComparison.OrdinalIgnoreCase))
+                {
+                    try
+                    {
+                        var jwtToken = new JwtSecurityTokenHandler()
+                            .ReadJwtToken(request.Headers.Authorization.Parameter);
+
+                        if (jwtToken.Payload.TryGetValue("oid", out var oidValue) && Guid.TryParse(oidValue.ToString(), out Guid oid))
+                            identity = oid;
+                    }
+                    catch
+                    {
+                        // swallow exceptions
+                    }
+                }
 
                 // the tracked rate limits are explained here:
                 // https://docs.microsoft.com/en-us/azure/azure-resource-manager/management/request-limits-and-throttling#remaining-requests
 
-                TrackRateLimit("remaining-subscription-reads");
-                TrackRateLimit("remaining-subscription-writes");
-                TrackRateLimit("remaining-tenant-reads");
-                TrackRateLimit("remaining-tenant-writes");
-                TrackRateLimit("remaining-subscription-resource-requests");
-                TrackRateLimit("remaining-subscription-resource-entities-read");
-                TrackRateLimit("remaining-tenant-resource-requests");
-                TrackRateLimit("remaining-tenant-resource-entities-read");
+                await Task.WhenAll
+                (
+                    TrackRateLimitAsync("remaining-subscription-reads", identity),
+                    TrackRateLimitAsync("remaining-subscription-writes", identity),
+                    TrackRateLimitAsync("remaining-tenant-reads", identity),
+                    TrackRateLimitAsync("remaining-tenant-writes", identity),
+                    TrackRateLimitAsync("remaining-subscription-resource-requests", identity),
+                    TrackRateLimitAsync("remaining-subscription-resource-entities-read", identity),
+                    TrackRateLimitAsync("remaining-tenant-resource-requests", identity),
+                    TrackRateLimitAsync("remaining-tenant-resource-entities-read", identity)
+                )
+                .ConfigureAwait(false);
 
-                void TrackRateLimit(string headerName)
+                Task TrackRateLimitAsync(string headerName, Guid identity)
                 {
-                    var headerValue = response.GetHeaderValue($"{headerPrefix}-{headerName}");
-                    if (headerValue is null || !double.TryParse(headerValue, out double rateLimit)) return;
+                    try
+                    {
+                        var headerValue = response.GetHeaderValue($"{headerPrefix}-{headerName}");
 
-                    var metricName = Regex.Replace(headerName, "(^|-)([a-z])",
-                        match => match.Value
-                        .Replace("-", " ", StringComparison.OrdinalIgnoreCase)
-                        .ToUpperInvariant());
+                        if (double.TryParse(headerValue, out double rateLimit))
+                        {
+                            var metricName = Regex.Replace(headerName, "(^|-)([a-z])",
+                                match => match.Value
+                                .Replace("-", " ", StringComparison.OrdinalIgnoreCase)
+                                .ToUpperInvariant());
 
-                    telemetryClient
-                        .GetMetric($"Azure Rate Limit {metricName}")
-                        .TrackValue(rateLimit);
+                            telemetryClient
+                                .GetMetric($"Azure Rate Limit {metricName}", "Identity")
+                                .TrackValue(rateLimit, identity.ToString());
+                        }
+                    }
+                    catch
+                    {
+                        // swallow exceptions
+                    }
+
+                    return Task.CompletedTask;
                 }
             }
 
