1dc803c757ea1d9b39b6db3459f4b33f9aafdffb
Colby Williams <ColbyLWilliams@gmail.com>
2020-05-29 11:38:24 -0400

    serialize ProviderCommandMode as string

--- src/TeamCloud.Model/Data/ProviderCommandMode.cs
+++ src/TeamCloud.Model/Data/ProviderCommandMode.cs
@@ -3,8 +3,12 @@
  *  Licensed under the MIT License.
  */
 
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
 namespace TeamCloud.Model.Data
 {
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum ProviderCommandMode
     {
         Simple,
