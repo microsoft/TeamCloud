4abf395edd40536b15ae312e0ed4cb8288226544
Markus Heiliger <markus.heiliger@microsoft.com>
2020-07-09 16:18:41 -0700

    renamed NullStorageArtifactsProvider to NoneStorageArtifactsProvider

--- src/TeamCloud.Azure.Deployment/Providers/NoneStorageArtifactsProvider.cs
+++ src/TeamCloud.Azure.Deployment/Providers/NoneStorageArtifactsProvider.cs
@@ -9,16 +9,16 @@ using System.Threading.Tasks;
 
 namespace TeamCloud.Azure.Deployment.Providers
 {
-    public sealed class NullStorageArtifactsProvider : AzureDeploymentArtifactsProvider
+    public sealed class NoneStorageArtifactsProvider : AzureDeploymentArtifactsProvider
     {
-        public static readonly IAzureDeploymentArtifactsProvider Instance = new NullStorageArtifactsProvider();
+        public static readonly IAzureDeploymentArtifactsProvider Instance = new NoneStorageArtifactsProvider();
 
         public override Task<string> DownloadArtifactAsync(Guid deploymentId, string artifactName)
             => Task.FromResult(default(string));
 
         public override Task<IAzureDeploymentArtifactsContainer> UploadArtifactsAsync(Guid deploymentId, AzureDeploymentTemplate azureDeploymentTemplate)
             => (azureDeploymentTemplate?.LinkedTemplates.Any() ?? false)
-            ? Task.FromException<IAzureDeploymentArtifactsContainer>(new NotSupportedException($"{nameof(NullStorageArtifactsProvider)} doesn't support linked templates or artifacts"))
+            ? Task.FromException<IAzureDeploymentArtifactsContainer>(new NotSupportedException($"{nameof(NoneStorageArtifactsProvider)} doesn't support linked templates or artifacts"))
             : Task.FromResult<IAzureDeploymentArtifactsContainer>(Container.Instance);
 
         internal sealed class Container : IAzureDeploymentArtifactsContainer
