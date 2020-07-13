b3fca990b301a57b57debbe89356d8884d75f1fd
Colby Williams <ColbyLWilliams@gmail.com>
2020-07-13 11:46:55 -0400

    revert updates to service principal creation for now

--- src/TeamCloud.Azure.Directory/AzureDirectoryService.cs
+++ src/TeamCloud.Azure.Directory/AzureDirectoryService.cs
@@ -10,9 +10,12 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using Flurl;
+using Flurl.Http;
 using Microsoft.Azure.Management.Graph.RBAC.Fluent;
 using Microsoft.Azure.Management.Graph.RBAC.Fluent.Models;
 using Microsoft.Rest.Azure.OData;
+using TeamCloud.Http;
 
 namespace TeamCloud.Azure.Directory
 {
@@ -216,137 +219,348 @@ namespace TeamCloud.Azure.Directory
 
         public async Task<AzureServicePrincipal> CreateServicePrincipalAsync(string name, string password = null)
         {
-            if (name is null)
-                throw new ArgumentNullException(nameof(name));
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Must not NULL or WHITESPACE", nameof(name));
 
             name = SanitizeServicePrincipalName(name);
 
-            using var client = azureSessionService
-                .CreateClient<GraphRbacManagementClient>(AzureEndpoint.GraphEndpoint);
-
-            password ??= CreateServicePrincipalPassword();
-
-            var expiresOn = DateTime.UtcNow.AddYears(1);
-
-            var parameters = new ApplicationCreateParameters()
+            try
             {
-                DisplayName = name,
-                IdentifierUris = new List<string> { $"http://{name}" },
-                Homepage = $"http://{name}",
-                PasswordCredentials = new List<PasswordCredential> {
-                    new PasswordCredential {
-                        StartDate = DateTime.UtcNow,
-                        EndDate = expiresOn,
-                        KeyId = Guid.NewGuid().ToString(),
-                        Value = password,
-                        CustomKeyIdentifier = Encoding.ASCII.GetBytes(name)
+                var servicePrincipal = await azureSessionService.CreateSession()
+                    .ServicePrincipals
+                    .Define(name)
+                        .WithNewApplication($"http://{name}")
+                        .CreateAsync()
+                        .ConfigureAwait(false);
+
+                return await ResetServicePrincipalAsync(name, password)
+                    .ConfigureAwait(false);
+            }
+            catch
+            {
+                // we created the service principal as part of this call
+                // as the password set operation failed we will try
+                // to clean up our mess
+
+                await DeleteServicePrincipalAsync(name)
+                    .ConfigureAwait(false);
+
+                throw;
+            }
+        }
+
+        public async Task<AzureServicePrincipal> ResetServicePrincipalAsync(string name, string password = null)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Must not NULL or WHITESPACE", nameof(name));
+
+            var serviceApplication = await GetServiceApplicationInternalAsync(name)
+                .ConfigureAwait(false);
+
+            if (serviceApplication is null)
+                throw new ArgumentOutOfRangeException(nameof(name));
+
+            var servicePrincipal = await GetServicePrincipalAsync(name)
+                .ConfigureAwait(false);
+
+            var token = await azureSessionService
+                .AcquireTokenAsync(AzureEndpoint.GraphEndpoint)
+                .ConfigureAwait(false);
+
+            if (string.IsNullOrEmpty(password))
+                password = CreateServicePrincipalPassword();
+
+            var startDate = DateTime.UtcNow;
+            var endDate = startDate.AddYears(1);
+
+            var patchPayloads = new List<object>()
+            {
+                new
+                {
+                    passwordCredentials = new[]
+                    {
+                        new
+                        {
+                            startDate,
+                            endDate,
+                            keyId = Guid.NewGuid(),
+                            value = password,
+                            customKeyIdentifier = Convert.ToBase64String(servicePrincipal.ObjectId.ToByteArray())
+                        }
                     }
                 },
-                RequiredResourceAccess = new List<RequiredResourceAccess> {
-                    new RequiredResourceAccess {
-                        ResourceAppId = "00000003-0000-0000-c000-000000000000",
-                        ResourceAccess = new List<ResourceAccess> {
-                            new ResourceAccess {
-                                Id = "e1fe6dd8-ba31-4d61-89e7-88639da4683d",
-                                Type = "Scope"
+                new
+                {
+                    requiredResourceAccess = new[]
+                    {
+                        new
+                        {
+                            resourceAppId = "00000003-0000-0000-c000-000000000000",
+                            resourceAccess = new[]
+                            {
+                                new
+                                {
+                                    id = "e1fe6dd8-ba31-4d61-89e7-88639da4683d",
+                                    type = "Scope"
+                                }
                             }
                         }
                     }
                 }
             };
 
-            try
-            {
-                var application = await client.Applications
-                    .CreateAsync(parameters)
-                    .ConfigureAwait(false);
+            var patchTasks = patchPayloads
+                .Select(payload => $"https://graph.windows.net/{azureSessionService.Options.TenantId}/applications/{serviceApplication.Inner.ObjectId}"
+                                    .SetQueryParam("api-version", "1.6")
+                                    .WithOAuthBearerToken(token)
+                                    .PatchJsonAsync(payload));
 
-                var principalId = await client.Applications
-                    .GetServicePrincipalsIdByAppIdAsync(application.AppId)
-                    .ConfigureAwait(false);
+            await Task
+                .WhenAll(patchTasks)
+                .ConfigureAwait(false);
 
-                var principal = await client.ServicePrincipals
-                    .GetAsync(principalId.Value)
-                    .ConfigureAwait(false);
+            servicePrincipal.Password = password;
 
-                var azureServicePrincipal = new AzureServicePrincipal()
-                {
-                    ObjectId = Guid.Parse(principal.ObjectId),
-                    ApplicationId = Guid.Parse(principal.AppId),
-                    Name = principal.ServicePrincipalNames.FirstOrDefault(),
-                    Password = password,
-                    ExpiresOn = expiresOn
-                };
-
-                return azureServicePrincipal;
-            }
-            catch (GraphErrorException ex)
-            {
-                throw new Exception(ex.Body.Message);
-                // throw;
-            }
+            return servicePrincipal;
         }
-
 
         public async Task<AzureServicePrincipal> GetServicePrincipalAsync(string name)
         {
-            if (name is null)
-                throw new ArgumentNullException(nameof(name));
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Must not NULL or WHITESPACE", nameof(name));
+
+            var servicePrincipal = await GetServicePrincipalInternalAsync(name)
+                .ConfigureAwait(false);
+
+            if (servicePrincipal is null)
+                return null;
+
+            var serviceApplication = await GetServiceApplicationInternalAsync(name)
+                .ConfigureAwait(false);
+
+            if (serviceApplication is null)
+                return null;
+
+            var azureServicePrincipal = new AzureServicePrincipal()
+            {
+                ObjectId = Guid.Parse(servicePrincipal.Id),
+                ApplicationId = Guid.Parse(servicePrincipal.ApplicationId),
+                Name = servicePrincipal.Name
+            };
+
+            var token = await azureSessionService
+                .AcquireTokenAsync(AzureEndpoint.GraphEndpoint)
+                .ConfigureAwait(false);
+
+            var json = await $"https://graph.windows.net/{azureSessionService.Options.TenantId}/applications/{serviceApplication.Inner.ObjectId}"
+                .SetQueryParam("api-version", "1.6")
+                .WithOAuthBearerToken(token)
+                .GetJObjectAsync()
+                .ConfigureAwait(false);
+
+            var identifier = Convert.ToBase64String(azureServicePrincipal.ObjectId.ToByteArray());
+            var expiresOn = json.SelectToken($"$.value[?(@.customKeyIdentifier == '{identifier}')].endDate")?.ToString();
+
+            if (!string.IsNullOrEmpty(expiresOn) && DateTime.TryParse(expiresOn, out var expiresOnDateTime))
+                azureServicePrincipal.ExpiresOn = expiresOnDateTime;
+
+            return azureServicePrincipal;
+        }
+
+        private async Task<IServicePrincipal> GetServicePrincipalInternalAsync(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Must not NULL or WHITESPACE", nameof(name));
 
             name = SanitizeServicePrincipalName(name);
 
-            using var client = azureSessionService
-                .CreateClient<GraphRbacManagementClient>(AzureEndpoint.GraphEndpoint);
-
-            var principal = await GetServicePrincipalInnerAsync(client, name)
-                .ConfigureAwait(false);
-
-            if (principal is null)
-                return null;
-
-            var application = await GetServiceApplicationInnerAsync(client, principal.AppId)
-                .ConfigureAwait(false);
-
-            if (application is null)
-                return null;
-
-            return new AzureServicePrincipal
+            try
             {
-                ObjectId = Guid.Parse(principal.ObjectId),
-                ApplicationId = Guid.Parse(principal.AppId),
-                Name = principal.ServicePrincipalNames.FirstOrDefault(),
-                ExpiresOn = application.PasswordCredentials.FirstOrDefault(c => c.CustomKeyIdentifier == Encoding.ASCII.GetBytes(name))?.EndDate
-            };
+                return await azureSessionService.CreateSession()
+                    .ServicePrincipals
+                    .GetByNameAsync(name)
+                    .ConfigureAwait(false);
+            }
+            catch
+            {
+                return null;
+            }
         }
 
+        private async Task<IActiveDirectoryApplication> GetServiceApplicationInternalAsync(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Must not NULL or WHITESPACE", nameof(name));
+
+            name = SanitizeServicePrincipalName(name);
+
+            try
+            {
+                return await azureSessionService.CreateSession()
+                    .ActiveDirectoryApplications
+                    .GetByNameAsync(name)
+                    .ConfigureAwait(false);
+            }
+            catch
+            {
+                return null;
+            }
+        }
 
         public async Task DeleteServicePrincipalAsync(string name)
         {
-            if (name is null)
-                throw new ArgumentNullException(nameof(name));
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Must not NULL or WHITESPACE", nameof(name));
 
-            name = SanitizeServicePrincipalName(name);
 
-            using var client = azureSessionService
-                .CreateClient<GraphRbacManagementClient>(AzureEndpoint.GraphEndpoint);
+            var session = azureSessionService.CreateSession();
 
-            var principal = await GetServicePrincipalInnerAsync(client, name)
+            var serviceApplication = await GetServiceApplicationInternalAsync(name)
                 .ConfigureAwait(false);
 
-            if (!(principal is null))
-            {
-                var application = await GetServiceApplicationInnerAsync(client, principal.AppId)
-                    .ConfigureAwait(false);
+            await session.ActiveDirectoryApplications.DeleteByIdAsync(serviceApplication.Id)
+                .ConfigureAwait(false);
 
-                if (!(application is null))
-                    await client.Applications
-                        .DeleteAsync(application.ObjectId)
-                        .ConfigureAwait(false);
+            var servicePrincipal = await GetServicePrincipalInternalAsync(name)
+                .ConfigureAwait(false);
 
-                await client.ServicePrincipals
-                    .DeleteAsync(principal.ObjectId)
-                    .ConfigureAwait(false);
-            }
+            await session.ServicePrincipals.DeleteByIdAsync(servicePrincipal.Id)
+                .ConfigureAwait(false);
         }
+
+        // public async Task<AzureServicePrincipal> CreateServicePrincipalAsync(string name, string password = null)
+        // {
+        //     if (name is null)
+        //         throw new ArgumentNullException(nameof(name));
+
+        //     name = SanitizeServicePrincipalName(name);
+
+        //     using var client = azureSessionService
+        //         .CreateClient<GraphRbacManagementClient>(AzureEndpoint.GraphEndpoint);
+
+        //     password ??= CreateServicePrincipalPassword();
+
+        //     var expiresOn = DateTime.UtcNow.AddYears(1);
+
+        //     var parameters = new ApplicationCreateParameters()
+        //     {
+        //         DisplayName = name,
+        //         IdentifierUris = new List<string> { $"http://{name}" },
+        //         Homepage = $"http://{name}",
+        //         PasswordCredentials = new List<PasswordCredential> {
+        //             new PasswordCredential {
+        //                 StartDate = DateTime.UtcNow,
+        //                 EndDate = expiresOn,
+        //                 KeyId = Guid.NewGuid().ToString(),
+        //                 Value = password,
+        //                 CustomKeyIdentifier = Encoding.ASCII.GetBytes(name)
+        //             }
+        //         },
+        //         RequiredResourceAccess = new List<RequiredResourceAccess> {
+        //             new RequiredResourceAccess {
+        //                 ResourceAppId = "00000003-0000-0000-c000-000000000000",
+        //                 ResourceAccess = new List<ResourceAccess> {
+        //                     new ResourceAccess {
+        //                         Id = "e1fe6dd8-ba31-4d61-89e7-88639da4683d",
+        //                         Type = "Scope"
+        //                     }
+        //                 }
+        //             }
+        //         }
+        //     };
+
+        //     try
+        //     {
+        //         var application = await client.Applications
+        //             .CreateAsync(parameters)
+        //             .ConfigureAwait(false);
+
+        //         var principalId = await client.Applications
+        //             .GetServicePrincipalsIdByAppIdAsync(application.AppId)
+        //             .ConfigureAwait(false);
+
+        //         var principal = await client.ServicePrincipals
+        //             .GetAsync(principalId.Value)
+        //             .ConfigureAwait(false);
+
+        //         var azureServicePrincipal = new AzureServicePrincipal()
+        //         {
+        //             ObjectId = Guid.Parse(principal.ObjectId),
+        //             ApplicationId = Guid.Parse(principal.AppId),
+        //             Name = principal.ServicePrincipalNames.FirstOrDefault(),
+        //             Password = password,
+        //             ExpiresOn = expiresOn
+        //         };
+
+        //         return azureServicePrincipal;
+        //     }
+        //     catch (GraphErrorException ex)
+        //     {
+        //         throw new Exception(ex.Body.Message);
+        //         // throw;
+        //     }
+        // }
+
+
+        // public async Task<AzureServicePrincipal> GetServicePrincipalAsync(string name)
+        // {
+        //     if (name is null)
+        //         throw new ArgumentNullException(nameof(name));
+
+        //     name = SanitizeServicePrincipalName(name);
+
+        //     using var client = azureSessionService
+        //         .CreateClient<GraphRbacManagementClient>(AzureEndpoint.GraphEndpoint);
+
+        //     var principal = await GetServicePrincipalInnerAsync(client, name)
+        //         .ConfigureAwait(false);
+
+        //     if (principal is null)
+        //         return null;
+
+        //     var application = await GetServiceApplicationInnerAsync(client, principal.AppId)
+        //         .ConfigureAwait(false);
+
+        //     if (application is null)
+        //         return null;
+
+        //     return new AzureServicePrincipal
+        //     {
+        //         ObjectId = Guid.Parse(principal.ObjectId),
+        //         ApplicationId = Guid.Parse(principal.AppId),
+        //         Name = principal.ServicePrincipalNames.FirstOrDefault(),
+        //         ExpiresOn = application.PasswordCredentials.FirstOrDefault(c => c.CustomKeyIdentifier == Encoding.ASCII.GetBytes(name))?.EndDate
+        //     };
+        // }
+
+
+        // public async Task DeleteServicePrincipalAsync(string name)
+        // {
+        //     if (name is null)
+        //         throw new ArgumentNullException(nameof(name));
+
+        //     name = SanitizeServicePrincipalName(name);
+
+        //     using var client = azureSessionService
+        //         .CreateClient<GraphRbacManagementClient>(AzureEndpoint.GraphEndpoint);
+
+        //     var principal = await GetServicePrincipalInnerAsync(client, name)
+        //         .ConfigureAwait(false);
+
+        //     if (!(principal is null))
+        //     {
+        //         var application = await GetServiceApplicationInnerAsync(client, principal.AppId)
+        //             .ConfigureAwait(false);
+
+        //         if (!(application is null))
+        //             await client.Applications
+        //                 .DeleteAsync(application.ObjectId)
+        //                 .ConfigureAwait(false);
+
+        //         await client.ServicePrincipals
+        //             .DeleteAsync(principal.ObjectId)
+        //             .ConfigureAwait(false);
+        //     }
+        // }
     }
 }
