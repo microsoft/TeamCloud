2da544486109ef4e4ab793ca94b55235379061ca
Markus Heiliger <markus.heiliger@microsoft.com>
2020-09-16 15:56:58 -0700

    run build in parallel

--- TeamCloud.cmd
+++ TeamCloud.cmd
@@ -1,20 +1,31 @@
 @echo off
+
+cls
+cd "%~dp0"
 set "lock=%temp%\wait%random%.lock"
 
-cd "%~dp0"
-start "" cmd /C azurestorageemulator start
-start /D .\src "" 9>"%lock%1" dotnet build --force -c Debug
-start /D .\web\teamcloud "" 9>"%lock%2" cmd /C npm install
+echo   ______                     ________                __
+echo  /_  __/__  ____ _____ ___  / ____/ /___  __  ______/ /
+echo   / / / _ \/ __ `/ __ `__ \/ /   / / __ \/ / / / __  / 
+echo  / / /  __/ /_/ / / / / / / /___/ / /_/ / /_/ / /_/ /  
+echo /_/  \___/\__,_/_/ /_/ /_/\____/_/\____/\__,_/\__,_/   
+echo.
 
-:: wait for a locks to be resolved - modify loop if more parallel
-:: tasks should be supported
+echo - Starting Azure Storage Emulator
+start "" cmd /C azurestorageemulator start
+
+echo - Building API and Orchestrator
+start /min /D .\src "" 9>"%lock%1" dotnet build --force -c Debug
+
+echo - Building Web UI
+start /min /D .\web\teamcloud "" 9>"%lock%2" cmd /C npm install
+
 :Wait 
 1>nul 2>nul ping /n 2 ::1
 for %%N in (1 2) do (
   (call ) 9>"%lock%%%N" || goto :Wait
 ) 2>nul
 
-:: do some cleanup work
 del "%lock%*"
 
 SET terminal=wt -d .\src\TeamCloud.API cmd /C "dotnet run --no-build" ;
