bc19d3c05e7895af4a50bdecafc9b74eaaabb21c
Markus Heiliger <markus.heiliger@microsoft.com>
2020-09-17 13:40:23 -0700

    added json converter for NameValueCollection

---
+++ src/TeamCloud.Serialization.Tests/SerializeNameValueCollectionTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Specialized;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using TeamCloud.Serialization.Converter;
+using Xunit;
+
+namespace TeamCloud.Serialization.Tests
+{
+    public class SerializeNameValueCollectionTests
+    {
+        [Fact]
+        public async Task DeSerialize_NameValueCollection()
+        {
+            var nvc = new NameValueCollection();
+
+            for (int i = 1; i <= 10; i++)
+                nvc.Add("foo", $"bar{i}");
+
+            var json = JsonConvert.SerializeObject(nvc, new NameValueCollectionConverter());
+
+            for (int i = 1; i <= 10; i++)
+                Assert.Contains($"bar{i}", json);
+
+            var nvc2 = JsonConvert.DeserializeObject<NameValueCollection>(json, new NameValueCollectionConverter());
+
+            for (int i = 1; i <= 10; i++)
+                Assert.Contains($"bar{i}", nvc2.GetValues("foo"));
+        }
+    }
+}
---
+++ src/TeamCloud.Serialization/Converter/NameValueCollectionConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace TeamCloud.Serialization.Converter
+{
+    public sealed class NameValueCollectionConverter : JsonConverter<NameValueCollection>
+    {
+        public override NameValueCollection ReadJson(JsonReader reader, Type objectType, [AllowNull] NameValueCollection existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            existingValue ??= new NameValueCollection();
+
+            serializer.Deserialize<Dictionary<string, string[]>>(reader)?
+                .SelectMany(item => item.Value.Select(val => new KeyValuePair<string, string>(item.Key, val)))
+                .ToList().ForEach(kvp => existingValue.Add(kvp.Key, kvp.Value));
+
+            return existingValue;
+        }
+
+        public override void WriteJson(JsonWriter writer, [AllowNull] NameValueCollection value, JsonSerializer serializer)
+        {
+            var dictionary = value?.AllKeys.ToDictionary(key => key, key => value.GetValues(key));
+
+            serializer.Serialize(writer, dictionary);
+        }
+    }
+}
