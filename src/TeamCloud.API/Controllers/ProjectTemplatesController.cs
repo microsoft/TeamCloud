22338019e0c7e4fc63340066e5b8249cf8dce80e
Colby Williams <ColbyLWilliams@gmail.com>
2020-11-18 17:03:28 -0500

    always get the updated project template from repo

--- src/TeamCloud.API/Controllers/ProjectTemplatesController.cs
+++ src/TeamCloud.API/Controllers/ProjectTemplatesController.cs
@@ -17,6 +17,7 @@ using TeamCloud.API.Data;
 using TeamCloud.API.Data.Results;
 using TeamCloud.API.Services;
 using TeamCloud.Data;
+using TeamCloud.Git.Services;
 using TeamCloud.Model.Commands;
 using TeamCloud.Model.Data;
 using TeamCloud.Model.Validation;
@@ -28,9 +29,13 @@ namespace TeamCloud.API.Controllers
     [Produces("application/json")]
     public class ProjectTemplatesController : ApiController
     {
-        public ProjectTemplatesController(UserService userService, Orchestrator orchestrator, IOrganizationRepository organizationRepository, IProjectTemplateRepository projectTemplateRepository)
+        private readonly IRepositoryService repositoryService;
+
+        public ProjectTemplatesController(UserService userService, Orchestrator orchestrator, IOrganizationRepository organizationRepository, IProjectTemplateRepository projectTemplateRepository, IRepositoryService repositoryService)
             : base(userService, orchestrator, organizationRepository, projectTemplateRepository)
-        { }
+        {
+            this.repositoryService = repositoryService ?? throw new ArgumentNullException(nameof(repositoryService));
+        }
 
 
         [HttpGet]
@@ -45,8 +50,12 @@ namespace TeamCloud.API.Controllers
                 .ToListAsync()
                 .ConfigureAwait(false);
 
+            var freshTemplates = await Task
+                .WhenAll(projectTemplates.Select(pt => repositoryService.UpdateProjectTemplateAsync(pt)))
+                .ConfigureAwait(false);
+
             return DataResult<List<ProjectTemplate>>
-                .Ok(projectTemplates)
+                .Ok(freshTemplates?.ToList() ?? projectTemplates)
                 .ToActionResult();
         });
 
@@ -58,12 +67,16 @@ namespace TeamCloud.API.Controllers
         [SwaggerResponse(StatusCodes.Status400BadRequest, "A validation error occured.", typeof(ErrorResult))]
         [SwaggerResponse(StatusCodes.Status404NotFound, "A ProjectTemplate with the projectTemplateId provided was not found.", typeof(ErrorResult))]
         [SuppressMessage("Usage", "CA1801: Review unused parameters", Justification = "Used by base class and makes signiture unique")]
-        public Task<IActionResult> Get(string projectTemplateId) => EnsureProjectTemplateAsync(projectTemplate =>
-        {
-            return DataResult<ProjectTemplate>
-                .Ok(projectTemplate)
-                .ToActionResult();
-        });
+        public Task<IActionResult> Get(string projectTemplateId) => EnsureProjectTemplateAsync(async projectTemplate =>
+       {
+           var freshTemplate = await repositoryService
+               .UpdateProjectTemplateAsync(projectTemplate)
+               .ConfigureAwait(false);
+
+           return DataResult<ProjectTemplate>
+               .Ok(freshTemplate ?? projectTemplate)
+               .ToActionResult();
+       });
 
 
         [HttpPost]
