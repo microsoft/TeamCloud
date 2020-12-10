5c67dea498605537d7a7202d9f7410f67667cf54
Colby Williams <ColbyLWilliams@gmail.com>
2020-12-09 23:24:07 -0500

    [web] refactoring nav subdirectory

---
+++ web/src/components/nav/index.ts
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export * from './OrgSettingsNav'
+export * from './ProjectNav'
+export * from './ProjectSettingsNav'
+export * from './RootNav'
