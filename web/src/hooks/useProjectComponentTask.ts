c2244aa1b85d00f1fddf53e0cb978d875922d149
Colby Williams <ColbyLWilliams@gmail.com>
2021-04-06 12:22:47 -0500

    Update useProjectComponentTask.ts

--- web/src/hooks/useProjectComponentTask.ts
+++ web/src/hooks/useProjectComponentTask.ts
@@ -7,7 +7,6 @@ import { useIsAuthenticated } from '@azure/msal-react';
 import { matchesRouteParam } from '../Utils';
 import { api } from '../API';
 import { useProject, useProjectComponent } from '.';
-// import { ComponentTask } from 'teamcloud';
 
 export const useProjectComponentTask = () => {
 
@@ -18,27 +17,10 @@ export const useProjectComponentTask = () => {
     const { data: project } = useProject();
     const { data: component } = useProjectComponent();
 
-    // const queryClient = useQueryClient();
-
     return useQuery(['org', project?.organization, 'project', project?.id, 'component', component?.id, 'componenttask', subitemId], async () => {
         const { data } = await api.getComponentTask(subitemId, project!.organization, project!.id, component!.id);
         return data;;
     }, {
         enabled: isAuthenticated && !!project?.id && !!component?.id && !!itemId && matchesRouteParam(component, itemId) && !!subitemId,
-        // onSuccess: data => {
-        //     if (data) {
-        //         const componentTasks = queryClient.getQueryData<ComponentTask[]>(['org', project?.organization, 'project', project?.id, 'component', component?.id, 'componenttask']);
-        //         if (componentTasks) {
-        //             const index = componentTasks.findIndex(t => t.id === data.id);
-        //             const newComponentTasks = [...componentTasks];
-        //             if (index >= 0) {
-        //                 newComponentTasks[index] = data;
-        //             } else {
-        //                 newComponentTasks.push(data);
-        //             }
-        //             queryClient.setQueryData(['org', project?.organization, 'project', project?.id, 'component', component?.id, 'componenttask'], newComponentTasks);
-        //         }
-        //     }
-        // }
     });
 }
