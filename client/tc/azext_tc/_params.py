507f8f68e4a19cba9ac87fd87164c38100e9b4fa
Colby Williams <ColbyLWilliams@gmail.com>
2021-06-29 22:23:26 -0500

    cleanup

--- client/tc/azext_tc/_params.py
+++ client/tc/azext_tc/_params.py
@@ -2,8 +2,8 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-
 # pylint: disable=too-many-statements
+
 from knack.arguments import CLIArgumentType
 from azure.cli.core.commands.parameters import (tags_type, get_enum_type)
 
